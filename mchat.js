01bdccf723aae4a94ded1c723b8766739fc5a292
Adrian Booth <Adrian.booth17@gmail.com>
2015-11-01 12:41:11 +0000

    Add event listener for enter key press in textarea

--- mchat.js
+++ mchat.js
@@ -1,22 +1,26 @@
 Messages = new Mongo.Collection('messages');
 
-
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
   Template.messages.helpers({
     messages: function () {
       return Messages.find();
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.messages.events({
+    'keypress textarea': function(e, instance){
+      if (e.keyCode == 13) { // enter key pressed
+        var value = instance.find('textarea').value;
+        instance.find('textarea').value = '';
+
+        Messages.insert({
+          message: value,
+          timestamp: new Date()
+          // user: Meteor.userId()
+        });
+      }
     }
-  });
+  })
 }
 
 if (Meteor.isServer) {
